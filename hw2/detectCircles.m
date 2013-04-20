a2f72a73e956ad0e65e9e617af09b14fc38d502e
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-04-20 14:05:19 +0300

    detectCircles: Simplify pre-processing
    Removed the filtering part, as it didn't seem to be required.

--- hw2/detectCircles.m
+++ hw2/detectCircles.m
@@ -13,23 +13,19 @@ function [ centers ] = detectCircles( im, radius, usegradient )
 %% Convert image to grayscale.
 im = rgb2gray(im);
 
-
 %% Create a smooth BW image of the edges:
-% * Convert image to BW with a variying threshold, and smooth it with a filter.
-% * Get edges using 'canny' method with high thresholds (since it's a BW image)
+% * Convert image to BW with a variying threshold.
+% * Get edges using 'canny' method with high thresholds (since it's a BW image).
 % * Accumulate edges.
 edges = false(size(im));
-diskFilter3 = fspecial('disk', 3);
+
 for bwThresh = [.1, .2, .4, .8]
-    bwCurrent = im2bw(im, bwThresh);
-    bwCurrent = imfilter(bwCurrent, diskFilter3);
-    
+    bwCurrent = im2bw(im, bwThresh); 
     edgesCurrent = edge(bwCurrent, 'canny', [.8, .9]);
     edges = edges | edgesCurrent;
 end
 
 %% DEBUG
-imshow(edges);
-figure; imshow(edge(edges));
+figure; imshow(edges);
 end
 
