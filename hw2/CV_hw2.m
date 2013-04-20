6095be57f5f2226d70a4d503fe92c3217233f98c
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-04-20 12:42:25 +0300

    Added HW2 main script and images

---
+++ hw2/CV_hw2.m
@@ -0,0 +1,17 @@
+%% Computer Vision, spring 2013
+%  
+%  Assignment 2
+%  Aviv Rosenberg
+
+%%
+% Load all images
+im_colorful1 = imread('./img/colorful1.jpg');
+im_colorful2 = imread('./img/colorful2.jpg');
+im_colorful3 = imread('./img/colorful3.png');
+im_ladybug = imread('./img/ladybug.jpg');
+im_mooncraters = imread('./img/MoonCraters.jpg');
+im_planets = imread('./img/Planets.jpeg');
+
+%% Question 1
+% 
+% 
