299dfbd1a8a8481639e5c6bf36009d3d5fbc0679
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-04-20 12:42:49 +0300

    Initial detectCircles
    The function now does some initial processing to generate a smooth BW
    image suitable for Hough transform.

--- hw2/detectCircles.m
+++ hw2/detectCircles.m
@@ -2,6 +2,18 @@ function [ centers ] = detectCircles( im, radius, usegradient )
 %DETECTCIRCLES Returns locations of circles with a given radius in an image
 %   Detailed explanation goes here
 
+im = rgb2gray(im);
+diskFilter3 = fspecial('disk', 3);
 
+edges = false(size(im));
+for bwThresh = [.1, .2, .4, .8]
+    bwCurrent = im2bw(im, bwThresh);
+    bwCurrent = imfilter(bwCurrent, diskFilter3);
+    
+    edgesCurrent = edge(bwCurrent, 'canny', [.8, .9]);
+    edges = edges | edgesCurrent;
 end
 
+imshow(edges);
+figure; imshow(edge(edges));
+end
