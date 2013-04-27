c198f10dcd9a7a51dbc18c729edb9e8f3732ecbd
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-04-27 10:32:46 +0300

    Renamed parser

--- hw2/detectCircles.m
+++ hw2/detectCircles.m
@@ -11,13 +11,13 @@ function [ centers ] = detectCircles( im, radius, usegradient, varargin )
 % 
 
 %% Paramters Parsing
-p = inputParser;
-addRequired(p,'im', @(x) ~isrow(x) && ~iscolumn(x) && ~isscalar(x));
-addRequired(p,'radius', @isscalar);
-addOptional(p,'usegradient', true, @islogical);
-addOptional(p,'quantization',0.25,@isscalar);
-addOptional(p,'hough_thresh',0.75,@isscalar);
-parse(p, im, radius, usegradient, varargin{:});
+parser = inputParser;
+addRequired(parser,'im', @(x) ~isrow(x) && ~iscolumn(x) && ~isscalar(x));
+addRequired(parser,'radius', @isscalar);
+addOptional(parser,'usegradient', true, @islogical);
+addOptional(parser,'quantization',0.25,@isscalar);
+addOptional(parser,'hough_thresh',0.75,@isscalar);
+parse(parser, im, radius, usegradient, varargin{:});
 
 %% Convert image to grayscale.
 im = rgb2gray(im);
@@ -41,7 +41,7 @@ end
 % * The hough plane accumulates these votes into bins.
 % * Bins with high values correspond to circle-centers in the image plane.
 
-q = p.Results.quantization; % Quantization factor
+q = parser.Results.quantization; % Quantization factor
 %Initialize Hough Plane, which is madeup of bins that accumulate the 'votes'.
 houghPlane = zeros(ceil(q * size(edges,1)), ceil(q * size(edges,2)));
 r = round(q * radius);
@@ -68,7 +68,7 @@ end
 % image plane.
 % These indices are where circles exists in the image.
 
-t = p.Results.hough_thresh; % Threshold percent for number of votes needed for a circle
+t = parser.Results.hough_thresh; % Threshold percent for number of votes needed for a circle
 
 [cX, cY] = find(houghPlane >= t * max(max(houghPlane)));
 cX = cX ./ q; cY = cY ./ q;
