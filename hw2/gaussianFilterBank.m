cfbff253ffac11568d16b8657ff6c2ff10709a67
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-05-05 22:32:56 +0300

    hw2: GaussianFilterBank impl.

---
+++ hw2/gaussianFilterBank.m
@@ -0,0 +1,34 @@
+function [ filterBank ] = gaussianFilterBank( angles, sizes, varargin )
+%GAUSSIANFILTERBANK Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Paramters Parsing
+parser = inputParser;
+parser.addRequired('angles', @(x) isrow(x) || iscolumn(x));
+parser.addRequired('sizes', @(x) isrow(x) || iscolumn(x));
+parser.addOptional('sigma', 1, @isscalar);
+parser.parse(angles, sizes, varargin{:});
+
+%% Create Filter Bank
+filterBank = cell(length(sizes), length(angles));
+bankRow = 0;
+
+ddx = fspecial('sobel')';
+ddy = fspecial('sobel');
+sigma = parser.Results.sigma;
+
+for sz=sizes
+    bankRow = bankRow + 1; bankCol = 0;
+    gx = conv2(fspecial('gaussian', sz, sigma), ddx, 'same');
+    gy = conv2(fspecial('gaussian', sz, sigma), ddy, 'same');
+    
+    for ang=angles
+        bankCol = bankCol + 1;
+        theta = ang * pi / 180;
+        filter = gx .* cos(theta) + gy .* sin(theta);
+        
+        filterBank{bankRow, bankCol} = filter;
+    end
+end
+
+end
