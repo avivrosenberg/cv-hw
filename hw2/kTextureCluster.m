1e99de756ebabff63bd533319120b60bce07549a
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-05-06 20:26:25 +0300

    kTextureCluster: Added support for RGB clustering

--- hw2/kTextureCluster.m
+++ hw2/kTextureCluster.m
@@ -8,30 +8,42 @@ parser.addParamValue('k',8, @isscalar);
 parser.addParamValue('angles',[0 60 120 180 240 300], @(x) isrow(x) || iscolumn(x));
 parser.addParamValue('sizes',[3 7 11], @(x) isrow(x) || iscolumn(x));
 parser.addParamValue('sigma',1, @isscalar);
+parser.addParamValue('rgb', false, @islogical);
 parser.parse(im, varargin{:});
 
 %% Apply filters to image
 fb = gaussianFilterBank(parser.Results.angles, parser.Results.sizes, 'sigma', parser.Results.sigma);
 
-if (size(im,3) > 1)
+if (size(im,3) > 1 && ~parser.Results.rgb)
     im = rgb2gray(im);
 end
 
-observations = zeros(numel(im), numel(fb));
+%% Nested function to create a clustered image
+    function im_clustered = clusterImage(im)
+        observations = zeros(numel(im), numel(fb));
+        
+        % Calculate responce to all filters
+        for i=1:numel(fb)
+            im_filtered = imfilter(im, fb{i});
+            observations(:,i) = im_filtered(:);
+        end
+        
+        % Cluster according to filter response
+        idx = kmeans(observations, parser.Results.k,...
+            'start','cluster',...
+            'emptyaction','drop');
+        
+        cMax = max(idx); cMin = min(idx);
+        % Use the indices themselves as pixel values for the output image.
+        im_clustered = reshape(idx, size(im,1), size(im,2));
+        % Scale the output.
+        im_clustered = uint8(255 .* (im_clustered - cMin) ./ (cMax - cMin));
+    end
 
-for i=1:numel(fb)
-    im_filtered = imfilter(im, fb{i});
-    observations(:,i) = im_filtered(:);
+%% Create output image: superimpose all cluster images
+out_im = uint8(zeros(size(im)));
+for j=1:size(im,3)
+    out_im(:,:,j) = clusterImage(im(:,:,j));
 end
 
-%% Cluster
-idx = kmeans(observations, parser.Results.k,...
-                  'start','cluster',...
-                  'emptyaction','drop');
-
-cMax = max(idx); cMin = min(idx);
-% Use the indices themselves as pixel values for the output image.
-out_im = reshape(idx, size(im,1), size(im,2));
-% Scale the output.
-out_im = uint8(255 .* (out_im - cMin) ./ (cMax - cMin));
 end
