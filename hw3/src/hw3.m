a8231520b19a2274adf5d8dc89b4fbbe319e95e6
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-06-08 19:57:17 +0300

    Renamed q2.m->hw3.m
    All question code will be in this file (except custom functions).
    
    Added script-temporary vars post-fixed by '__' (double underscore),
    as opposed to section-temporary vars post-fixed by '_' (single underscore).

--- hw3/src/hw3.m
+++ hw3/src/hw3.m
@@ -1,5 +1,5 @@
 %% Computer Vision, spring 2013
-%  
+%
 %  Assignment 3
 %  Aviv Rosenberg
 
@@ -8,22 +8,27 @@
 % o Should be in the MATLAB path
 % o Should be compiled
 
-%% Load all images as grayscale
-% vars ending with '_' are temporary.
 close all;
 clear;
+
+% NOTE: vars ending with '_' are temporary.
+framesPrefix__ = 'frames';
+descriptorsPrefix__ = 'descr';
+imVarNames__ = {};
+
+%% Load all images as grayscale
+%
 basepath_ = fileparts(mfilename('fullpath'));
 imfolder_ = [basepath_ '/../img/'];
 imageFiles_ = strsplit(ls(imfolder_));
 imageFiles_ = imageFiles_(1:end-1);
-imVarNames = {};
 
 for imFileName_ = imageFiles_
     % build a variable name in matlab wrokspace.
     varname_ = strsplit(imFileName_{:},'.');
     varname_ = varname_{1};
     varname_ = ['im' genvarname(varname_)];
-    imVarNames = [imVarNames {varname_}];
+    imVarNames__ = [imVarNames__ {varname_}];
     
     % build full path to the image file
     filepath_ = [imfolder_ imFileName_{:}];
@@ -33,9 +38,9 @@ for imFileName_ = imageFiles_
     im_ = (im_ - min(im_(:))) / max(im_(:));
     
     % set image into a new variable named according to 'varname'.
-    assignin('base', varname_, im_); 
+    assignin('base', varname_, im_);
 end
-clearvars *_;
+clearvars *_ -except *__;
 
 %% Apply SIFT
 %
