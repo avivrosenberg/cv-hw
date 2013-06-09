7f639b022ad1335a2d9039ea68e02083589206db
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-06-09 21:09:24 +0300

    Bugfix in RANSAC's dist function
    Added code for hnormalize() to scale homogeneous coordinate.
    Also removed file for normalise2dpts(), moved it to the helper functions
    in transformationMatches().

--- hw3/src/transformationMatches.m
+++ hw3/src/transformationMatches.m
@@ -1,4 +1,4 @@
-function [tform, inliers, error] = transformationMatches(matches1, matches2, varargin)
+function [tform, inliers, totalError] = transformationMatches(matches1, matches2, varargin)
 %TRANSFORMATIONMATCHES Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -9,7 +9,7 @@ parser.addRequired('matches1', @(x) ndims(x) == 2 && size(x,1) == 2);
 parser.addRequired('matches2', @(x) ndims(x) == 2 && size(x,1) == 2);
 parser.addParamValue('TransformType', 'affine', @(x) strcmp(x,'affine') || strcmp(x,'homography'));
 parser.addParamValue('MaxIterations', 1000, @isscalar);
-parser.addParamValue('MinSquareDistThresh', 0.7, @isscalar);
+parser.addParamValue('MinSquareDistThresh', 0.25, @isscalar);
 parser.addParamValue('MinInliersPerTransform', 5, @isscalar);
 
 parser.parse(matches1, matches2, varargin{:});
@@ -43,7 +43,7 @@ bestInliers = zeros(1,nPoints);
 matches1 = [matches1; ones(1,nPoints)];
 matches2 = [matches2; ones(1,nPoints)];
 
-% normalize points
+% shift and scale points so they are evenly distanced from origin
 [matches1, T1] = normalise2dpts(matches1);
 [matches2, T2] = normalise2dpts(matches2);
 %% RANSAC
@@ -70,7 +70,7 @@ end
 
 % save outputs
 inliers = bestInliers;
-error = minDist;
+totalError = minDist;
 
 % denormalize
 tform = T2\H*T1;
@@ -79,12 +79,66 @@ end
 
 %% Helper functions
 %
+
+% Error function for a a given transformation.
 function [error, inliers] = calcTransformDist(x1, x2, H, th)
         
-    % transform x2 with H and check distance from x1
-    diff2 = (x1 - H * x2).^2;
+    % transform x1 with H (and normalize homogeneous coordinates)
+    x2New = hnormalise( H * x1 );
+    
+    % check distance of new x2 from original x2
+    diff2 = (x2New - x2).^2;
     error = sqrt(sum(diff2,1));
     
     inliers = error < th;
     error = sum(error);
+end
+
+% Credit: The following functions were authored by
+% % Peter Kovesi
+% % http://www.csse.uwa.edu.au/~pk
+
+% Normalize homogeneous coordinates so that w = 1
+function nx = hnormalise(x)
+    [rows,~] = size(x);
+    nx = x;
+
+    % Find the indices of the points that are not at infinity
+    finiteind = find(abs(x(rows,:)) > eps);
+
+    for r = 1:rows-1
+        nx(r,finiteind) = x(r,finiteind)./x(rows,finiteind);
+    end
+    nx(rows,finiteind) = 1;
+end
+
+% Shift and scale points so that they're centered at origin with a 
+% mean distance of sqrt(2).
+function [newpts, T] = normalise2dpts(pts)
+    if size(pts,1) ~= 3
+        error('pts must be 3xN');
+    end
+
+    % Find the indices of the points that are not at infinity
+    finiteind = find(abs(pts(3,:)) > eps);
+    
+    % For the finite points ensure homogeneous coords have scale of 1
+    pts(1,finiteind) = pts(1,finiteind)./pts(3,finiteind);
+    pts(2,finiteind) = pts(2,finiteind)./pts(3,finiteind);
+    pts(3,finiteind) = 1;
+    
+    c = mean(pts(1:2,finiteind)')';            % Centroid of finite points
+    newp(1,finiteind) = pts(1,finiteind)-c(1); % Shift origin to centroid.
+    newp(2,finiteind) = pts(2,finiteind)-c(2);
+    
+    dist = sqrt(newp(1,finiteind).^2 + newp(2,finiteind).^2);
+    meandist = mean(dist(:));
+    
+    scale = sqrt(2)/meandist;
+    
+    T = [scale   0   -scale*c(1)
+         0     scale -scale*c(2)
+         0       0      1      ];
+    
+    newpts = T*pts;
 end
