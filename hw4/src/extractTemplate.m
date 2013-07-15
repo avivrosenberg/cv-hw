8a838fb40ff7079eb9fd349dc8fd1ffb08150406
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-07-15 23:39:18 +0300

    Initial code for extractTemplate
    This function will eventually extract a template image from a given test
    image.

---
+++ hw4/src/extractTemplate.m
@@ -0,0 +1,39 @@
+function [ im_out ] = extractTemplate( im_template, im_test, varargin )
+%EXTRACTTEMPLATE Find a given template image in a test image
+%   Detailed explanation goes here
+
+%% Input parsing
+%
+
+featFunc = @(im) sift(im, 'FirstOctave', 0);
+
+%% Features calculation
+% 
+% Extract frames (location, size, orientation of keypoints) and the
+% descriptors for each keypoint.
+
+[templFrames, templDescr] = featFunc( im_template );
+[testFrames, testDescr] = featFunc( im_test );
+
+%% Features matching
+%
+% Match descriptors using Lowe's method.
+% Use RANSAC to find an affine transform between the matching point, and
+% discard all matches not fitting the transform.
+
+matches = findMatches(templDescr, testDescr);
+
+% extract image coordinates of the matches.
+pts1 = templFrames(1:2,matches(1,:));
+pts2 = testFrames (1:2,matches(2,:));
+
+% find affine transform from matches 
+[~, inliers, ~] = transformationMatches(pts1, pts2, 'TransformType', 'affine');
+
+% use transform's inliers to filter out bad matches
+matches = matches(:, inliers);
+pts1 = templFrames(1:2,matches(1,:));
+pts2 = testFrames (1:2,matches(2,:));
+
+end
+
