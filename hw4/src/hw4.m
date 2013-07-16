d2ff8dcd92d9a09294b487ffeef2ebae3fceb642
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-07-16 23:11:49 +0300

    hw4.m: Load image sets
    Use the ImageSet class to load all sets into memory.

--- hw4/src/hw4.m
+++ hw4/src/hw4.m
@@ -3,5 +3,15 @@
 %  Assignment 4
 %  Aviv Rosenberg
 
-%%
+%% Load Image Sets
 %
+close all;
+clear;
+
+basepath = fileparts(mfilename('fullpath'));
+imfolder = [basepath '/../img/'];
+imHeight = 500; % desired height in pixels to load the images in
+
+supermanSet = ImageSet([imfolder 'Superman'], imHeight);
+starbucksSet = ImageSet([imfolder 'Starbucks'], imHeight);
+roadsignSet = ImageSet([imfolder 'Roadsign'], imHeight);
