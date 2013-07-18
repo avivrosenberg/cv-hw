2cdc4d771a16c01d86b51fdd87cdf4dc85ccbf5e
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-07-18 09:14:24 +0300

    Implemented voting for keypoints
    The 'keypointVotes' function decides which of the keypoints are actually
    on the object, by having each keypoint vote for the object's center and
    taking the keypoints that voted with the majority.

--- hw4/src/extractTemplate.m
+++ hw4/src/extractTemplate.m
@@ -1,4 +1,4 @@
-function [ im_out ] = extractTemplate( im_template, im_test, varargin )
+function [ im_out ] = extractTemplate( im_tmpl, im_test, varargin )
 %EXTRACTTEMPLATE Find a given template image in a test image
 %   Detailed explanation goes here
 
@@ -12,8 +12,8 @@ featFunc = @(im) sift(im);
 % Extract frames (location, size, orientation of keypoints) and the
 % descriptors for each keypoint.
 
-[templFrames, templDescr] = featFunc( im_template );
-[testFrames, testDescr] = featFunc( im_test );
+[kp_tmpl, desc_tmpl] = featFunc( im_tmpl );
+[kp_test, desc_test] = featFunc( im_test );
 
 %% Features matching
 %
@@ -21,24 +21,33 @@ featFunc = @(im) sift(im);
 % Use RANSAC to find an affine transform between the matching point, and
 % discard all matches not fitting the transform.
 
-matches = findMatches(templDescr, testDescr, 'thresh', 0.8);
+matches = findMatches(desc_tmpl, desc_test, 'thresh', 0.8);
+
+% Use voting for image center to determine which of the matches are on the object
+[votes, vote_results] = keypointVotes(im_tmpl, kp_tmpl(:, matches(1,:)), kp_test(:, matches(2,:)), ...
+                                      'clustertype','ward','maxclusters',7);
+
+% Get new matches
+matches = matches(:, vote_results);
 
 % extract image coordinates of the matches.
-pts1 = templFrames(1:2,matches(1,:));
-pts2 = testFrames (1:2,matches(2,:));
+pts1 = kp_tmpl(1:2,matches(1,:));
+pts2 = kp_test (1:2,matches(2,:));
 
 % find affine transform from matches 
 [~, inliers, ~] = transformationMatches(pts1, pts2, 'TransformType', 'affine');
+
 % use transform's inliers to filter out bad matches
 matches = matches(:, inliers);
 
-pts1 = templFrames(1:2,matches(1,:));
-pts2 = testFrames (1:2,matches(2,:));
-
 %% DEBUG
-%
-%
 
-plotMatches(im_template,im_test,templFrames(1:2,:),testFrames(1:2,:),matches,'Placement','horz');
+% Show votes
+figure;
+imshow(im_test); hold on; scatter(votes(1,vote_results),votes(2,vote_results),'r+');
+hold off;
+
+figure;
+plotMatches(im_tmpl,im_test,kp_tmpl(1:2,:),kp_test(1:2,:),matches,'Placement','horz');
 end
 
---
+++ hw4/src/keypointVotes.m
@@ -0,0 +1,90 @@
+function [ votes, voting_result ] = keypointVotes( im_tmpl, kp_tmpl, kp_test, varargin)
+%KEYPOINTVOTES Determines which keypoints from the test image belong to the
+%object, by using a voting system.
+%   im_tmpl - teplate image showing the desired object
+%   fr_tmpl - 4xK matrix of keypoints in the template image
+%   im_test - test image we would like to find the object in
+%   fr_test - 4xK matrix of keyopints in the test image
+%
+%   The given keypoints are assumed to be MATCHING.
+%
+%   The function returns:
+%       votes - 2xK matrix of the x,y location each keypoint in the test
+%       image voted for as the test image's center.
+%
+%   This function is based on:
+%       A. Suga, K. Fukuda, T. Takiguchi, Y. Ariki
+%       "Object Recognition and Segmentation Using SIFT and Graph Cuts"
+%       2008
+
+%% Paramters Parsing
+parser = inputParser;
+parser.addRequired('im_tmpl', @ismatrix);
+parser.addRequired('kp_tmpl', @ismatrix);
+parser.addRequired('kp_test', @ismatrix);
+parser.addParamValue('clustertype', 'ward', @(x) strcmp(x,'ward') || strcmp(x,'kmeans'));
+parser.addParamValue('maxclusters', 5, @isscalar);
+
+parser.parse(im_tmpl, kp_tmpl, kp_test, varargin{:});
+%% Extract keypoint data
+
+% frame x/y values are ZERO based, so add 1
+x_tmpl = kp_tmpl(1,:) + 1;
+y_tmpl = kp_tmpl(2,:) + 1;
+x_test = kp_test(1,:) + 1;
+y_test = kp_test(2,:) + 1;
+
+scale_tmpl = kp_tmpl(3,:);
+scale_test = kp_test(3,:);
+
+theta_tmpl = kp_tmpl(4,:);
+theta_test = kp_test(4,:);
+
+%% Template image center
+
+% in images, y corresonds to ROW and x to COL.
+[n1, m1] = size(im_tmpl);
+yC = floor(n1 * 0.5);
+xC = floor(m1 * 0.5);
+
+% Offsets of template keypoints from center
+dx = x_tmpl - xC;
+dy = y_tmpl - yC;
+
+%% Votes Computation
+
+% absolute angle of each template keypoint LOCATION (not orientation) relative image center
+theta = atan(dy ./ dx);
+
+% absolute distance of each template keypoint LOCATION from the image center
+r = sqrt(dx.^2 + dy.^2);
+
+% ratios of scale between test and template image keypoints
+scale_ratio = scale_test ./ scale_tmpl;
+
+% delta of keypoint ORIENTATIONS between test and template
+dtheta = theta_tmpl - theta_test;
+
+votesX = x_test + scale_ratio .* r .* cos(theta + dtheta);
+votesY = y_test + scale_ratio .* r .* sin(theta + dtheta);
+votes = [votesX; votesY];
+
+%% Cluster votes
+
+switch (parser.Results.clustertype)
+    case 'ward'
+        clusters_idx = clusterdata(votes','linkage','ward', 'maxclust', parser.Results.maxclusters);
+        
+    case 'kmeans'
+        clusters_idx = kmeans(votes', parser.Results.maxclusters, ...
+                              'emptyaction','drop', 'start','cluster');
+end
+
+% actual voting: select the cluster with the most members
+largest_cluster = mode(clusters_idx);
+
+% the voting results is a logical indexing of the keypoints that belong to
+% the cluster that received the most votes.
+voting_result = clusters_idx == largest_cluster;
+end
+
