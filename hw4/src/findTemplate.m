6c37f2e5d221b4d9a81cb511038e92a7522ab139
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-07-18 11:09:35 +0300

    findTemplate: Find template in test image
    The extractTemplate function was re-written into findTemplate, which
    does not extract, but only returns points of the object which were found
    in the test image.
    
    Also:
        * Added input parameters for configuration
        * Revised the debug-plots
        * Wrote documentation

---
+++ hw4/src/findTemplate.m
@@ -0,0 +1,95 @@
+function [ template_points_in_test, tform ] = findTemplate( im_tmpl, im_test, varargin )
+%EXTRACTTEMPLATE Find a given template image in a test image.
+%   Given a template image (IM_TMPL), and a test image (IM_TEST) the
+%   function tries to find the template in the test.
+%
+%   The template image is assumed to contain a single object, and the test
+%   image could be anything.
+%
+%   The function returns coordinates of points in the test image that have
+%   been found be be in an instance of the object in the test image.
+%   Also, a transformation matrix (TFORM) is returns such that IM_TMP *
+%   TFORM should give the entire object's location in IM_TEST.
+
+%% Paramters Parsing
+parser = inputParser;
+parser.addRequired('im_tmpl', @(x) ismatrix(x) && ndims(x) == 2);
+parser.addRequired('im_test', @(x) ismatrix(x) && ndims(x) == 2);
+parser.addParamValue('clustertype', 'kmeans', @(x) strcmp(x,'ward') || strcmp(x,'kmeans'));
+parser.addParamValue('maxclusters', 7, @isscalar);
+parser.addParamValue('matchthresh', 0.9, @isscalar);
+parser.addParamValue('transformtype', 'affine', @(x) strcmp(x,'affine') || strcmp(x,'homography'));
+
+parser.parse(im_tmpl, im_test, varargin{:});
+
+featFunc = @(im) sift(im);
+
+%% Features calculation
+% 
+% Extract frames (location, size, orientation of keypoints) and the
+% descriptors for each keypoint.
+
+[kp_tmpl, desc_tmpl] = featFunc( im_tmpl );
+[kp_test, desc_test] = featFunc( im_test );
+
+%% Descripor matching
+%
+% Match descriptors using Lowe's method.
+% Use RANSAC to find an affine transform between the matching point, and
+% discard all matches not fitting the transform.
+% matches will contain indices for kp_tmpl in row 1, and indices for kp_test in row2.
+matches = findMatches(desc_tmpl, desc_test, 'thresh', parser.Results.matchthresh);
+
+% Extract matching keypoints from both images (including scale and
+% orientation data).
+matching_kp_tmpl = kp_tmpl(:, matches(1,:));
+matching_kp_test = kp_test(:, matches(2,:));
+
+%% Voting for template object's center
+%
+% Use voting for image center to determine which of the matches are on the object.
+% votes contains the calculated center location of the object in the test
+% image, as calculated from each keypoint in matching_kp_test.
+[votes, vote_results] = keypointVotes(im_tmpl, ...
+                                      matching_kp_tmpl, matching_kp_test, ...
+                                      'clustertype',parser.Results.clustertype, ...
+                                      'maxclusters',parser.Results.maxclusters);
+                                  
+% Take only the votes (object center locations) which are in the largest
+% cluster of center locations.
+majority_votes = votes(:, vote_results);
+                                  
+% Get new matches: Take only matching keypoints which voted with the
+% majority on the location of the object.
+matches = matches(:, vote_results);
+
+%% Filter matching keypoint based on image transform and it's inliers
+%
+% Extract image coordinates of the new matching keypoints.
+pts1 = kp_tmpl(1:2,matches(1,:));
+pts2 = kp_test (1:2,matches(2,:));
+
+% Find image transform from keypoint locations in both images.
+[tform, inliers, ~] = transformationMatches(pts1, pts2, 'TransformType', parser.Results.transformtype);
+
+% use transform's inliers to filter matches even further.
+% matches will now contain the keypoint indicies of keypoints which votes
+% with the majority on the object center AND ALSO conform with the affine transform. 
+matches = matches(:, inliers);
+
+% extract keypoint locations in the test image using the new matches.
+% these keypoint location are assumed to all be inside the desired object,
+% and can therefor be use for segmentation of the object.
+template_points_in_test = kp_test (1:2,matches(2,:));
+%% DEBUG
+
+% Show votes
+figure('Name','Keypoint Votes: Computed center locations');
+imshow(im_test); hold on;
+scatter(majority_votes(1,:),majority_votes(2,:),'r+');
+scatter(majority_votes(1,inliers),majority_votes(2,inliers),'go');
+scatter(template_points_in_test(1,:),template_points_in_test(2,:),'m.');
+legend('Majority-vote keypoints','Majority-vote & Affine keypoints', 'Actual Keypoints');
+hold off;
+end
+
