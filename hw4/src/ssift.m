c534e0524c2222f79b1a6f89271686159ea80e49
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-07-27 23:13:27 +0300

    SSIFT: Better implementation, with scale clustering
    The SSIFT descriptor now uses the SIFT keypoint's sigma to rescale the
    images before computing the Self Similarity descriptor.
    This attempts to calculate all the SS descriptors at a uniform scale.

--- hw4/src/ssift.m
+++ hw4/src/ssift.m
@@ -1,10 +1,16 @@
 function [ keypoints, descriptors ] = ssift( im_color, varargin )
-%SSIFT Summary of this function goes here
-%   Detailed explanation goes here
+%SSIFT Self-Similarity SIFT descriptor
+%   This function computes the SSIFT keypoints and descriptors for a given
+%   image.
+%   The SSIFT descriptors uses SIFT to find scale invariant keypoints, but
+%   then calculates the Self-Similarity descriptor at these keypoints while
+%   accounting for scale (the Self-Similarity descriptor itself does not
+%   account for scale).
 
 %% Parameter Parsing
 parser = inputParser;
 parser.addRequired('im_color', @(x) ~isrow(x) && ~iscolumn(x) && ~isscalar(x) && ~isempty(x));
+parser.addParamValue('scaleclusters',5, @isscalar);
 parser.addParamValue('patch_size',5, @isscalar);
 parser.addParamValue('desc_rad',40, @isscalar);
 parser.addParamValue('nrad',3, @isscalar);
@@ -28,7 +34,7 @@ params.homogeneity_thresh = parser.Results.homogeneity_thresh;
 params.snn_thresh = parser.Results.snn_thresh;
 
 %%
-[rows, cols, ncolors] = size(im_color);
+[rows, cols, ~] = size(im_color);
 
 %% SIFT
 
@@ -44,42 +50,63 @@ sift_kp_xy = round(sift_kp(1:2,:)) + 1; % Add one because sift keypoints are zer
 
 im = double(im_color);
 
-% calculate the margin in which SSIM descriptors can't be calculated
+% cluster sift keypoints according to their sigma (proportional to radius
+% of region around the keypoint).
+[sigma_idx, sigma] = kmeans(sift_kp(3,:)', parser.Results.scaleclusters, 'EmptyAction', 'drop');
+sigma = sigma';
+sigma_idx = sigma_idx';
+
+% scale each keypoint xy coordinate and replace sigma with clustered values
+% these will be used as the keypoints for self similarity
+ss_kp = [
+    round(sift_kp_xy(1,:) ./ sigma(sigma_idx));
+    round(sift_kp_xy(2,:) ./ sigma(sigma_idx));
+    sigma(sigma_idx);
+    ];
+
+% remove keypoints which are located in the margin of their scaled image
+% (the self similarity descriptor cannot be calculated for these).
 margin = params.desc_rad + (params.patch_size - 1)./2;
-xmargins = [margin + 1, cols - margin];
-ymargins = [margin + 1, rows - margin];
 
-% keypoints inside the margin need to be removed.
-bad_kp_x_idx = (sift_kp_xy(1,:) < xmargins(1)) | (sift_kp_xy(1,:) > xmargins(2));
-bad_kp_y_idx = (sift_kp_xy(2,:) < ymargins(1)) | (sift_kp_xy(2,:) > ymargins(2));
-good_kp_idx = ~bad_kp_x_idx & ~bad_kp_y_idx;
+scaled_rows = floor(rows ./ ss_kp(3,:));
+scaled_cols = floor(cols ./ ss_kp(3,:));
 
+bad_kp_x_idx = (ss_kp(1,:) < margin + 1) | (ss_kp(1,:) > scaled_cols - margin);
+bad_kp_y_idx = (ss_kp(2,:) < margin + 1) | (ss_kp(2,:) > scaled_rows - margin);
+good_kp_idx = ~(bad_kp_x_idx | bad_kp_y_idx);
+
+% throw away bad keypoints and their descriptors
 sift_kp = sift_kp(:, good_kp_idx);
 sift_desc = sift_desc(:, good_kp_idx);
-sift_kp_xy = sift_kp_xy(:, good_kp_idx);
+ss_kp = ss_kp(:, good_kp_idx);
+sigma_idx = sigma_idx(:, good_kp_idx);
+
+% scale the input image K times
+im_scaled = cell(1, length(sigma));
+for i=1:length(sigma)
+    if (isnan(sigma(i))), continue; end
+    im_scaled{i} = imresize(im, 1/sigma(i));
+end
 
 % iterate over keypoints, and calculate self-similarity
 nkp = size(sift_kp, 2);
 ss_desc = zeros(params.nrad * params.nang, nkp);
 for k=1:nkp
-    x = sift_kp_xy(1, k);
-    y = sift_kp_xy(2, k);
+    x = ss_kp(1, k);
+    y = ss_kp(2, k);
     
-    [ss_desc_xy, ~, ~, ~, ~] = mexCalcSsdescs(im, params,  [ x y ]);
+    [ss_desc_xy, ~, ~, ~, ~] = mexCalcSsdescs(im_scaled{sigma_idx(k)}, params,  [ x y ]);
     ss_desc(:, k) = ss_desc_xy;
 end
 
-% normalize
-%ssim_desc = mat2gray(ssim_desc) * max(sift_desc(:));
+%% Results
 
-%% 
-
-
+% take keypoint locations in the original scale
 keypoints = sift_kp;
-%descriptors = [sift_desc; ss_desc];
-descriptors = ss_desc;
-%weights = [2*ones(1, size(sift_desc,1)), 1*ones(1, ncolors*nfilters)];
-%[~, pca_desc, ~] = pca([sift_desc; filt_desc]', 'NumComponents', 200, 'VariableWeights', weights);
-%descriptors = pca_desc';
+
+% normalize all descriptors to [0,1]
+%descriptors = [mat2gray(sift_desc); mat2gray(ss_desc)];
+descriptors = mat2gray(ss_desc);
+
 end
 
